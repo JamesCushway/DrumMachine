1c5d6395f0c7e4fa0b4284e23e40f10cc786ddf0
JamesCushway <jamespcushway@gmail.com>
2016-09-06 10:18:13 +0200

    DAC output of square, triangle and sin wave of 1khz. Mapped to GPIO input pins

--- DAC_PUSHBUTTONS/main.c
+++ DAC_PUSHBUTTONS/main.c
@@ -1,114 +1,338 @@
-//****************************************************
-//
-//   EXAMPLE OF USING CS42L22 on STM32F4-Discovery
-//
-//   author: A.Finkelmeyer
-//   http://www.mind-dump.net/configuring-the-stm32f4-discovery-for-audio
-//
-//****************************************************
+/**
+*****************************************************************************
+**
+**  File        : ECSA ELO PRAC - DAY 4
+**
+**  Abstract    : main function.
+**
+**  Functions   : main
+**
+**  Environment : Atollic TrueSTUDIO(R)
+**                STMicroelectronics STM32F4xx Standard Peripherals Library
+**
+**  Distribution: The file is distributed “as is,” without any warranty
+**                of any kind.
+**
+**  (c)Copyright Atollic AB.
+**  You may use this file as-is or modify it according to the needs of your
+**  project. Distribution of this file (unmodified or modified) is not
+**  permitted. Atollic AB permit registered Atollic TrueSTUDIO(R) users the
+**  rights to distribute the assembled, compiled & linked contents of this
+**  file as part of an application binary file, provided that it is built
+**  using the Atollic TrueSTUDIO(R) toolchain.
+**
+**
+*****************************************************************************
+*/
 
-#include "main.h"
+/* Includes */
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
+#include <math.h>
+
+/* Private macro */
+#define DACBUFFERSIZE 		256
+#define WAVEFREQ			1000 /* 1KHz */
+#define TIMER6_PRESCALER	2 	/* produces a 42MHz tick */
+#define TIMER_CLOCK			84E6 /* TIM6 runs at 84MHz */
+/* Private variables */
+uint16_t DACBuffer1[DACBUFFERSIZE]; 	/* Array for  waveform 1*/
+uint16_t DACBuffer2[DACBUFFERSIZE]; 	/* Array for  waveform 2*/
+uint16_t DACBuffer3[DACBUFFERSIZE];
+uint16_t DACBuffer4[DACBUFFERSIZE];
+/* Private function prototypes */
+void RCC_Configuration(void);
+void DMA_Configuration( uint16_t* wavBuffer );
+void NVIC_Configuration(void);
+void GPIO_Configuration(void);
+void UART_Configuration(void);
+void Timer_Configuration(uint16_t wavPeriod, uint16_t preScaler);
+void DAC_Configuration(void);
+void delay_ms(uint32_t milli);
+
+/*******************************************************************************
+* Function Name  : TIM2_IRQHandler
+* Description    : This function handles TIM2 global interrupt request.
+* Input          : None
+* Output         : None
+* Return         : None
+*******************************************************************************/
+void TIM2_IRQHandler(void)
+{
+  if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+    //Do stuff here
+  }
+}
 
 
+/**
+**===========================================================================
+**
+**  Abstract: main program
+**
+**===========================================================================
+*/
 int main(void)
 {
-	SystemInit();
+	STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_GPIO);
+	uint32_t fTimer;
+	uint32_t timerFreq;
+	uint16_t timerPeriod;
+	uint16_t n;
+	uint16_t m;
+	uint32_t buttonVal;
+	uint8_t buffNum = 1;
 
-	fir_8 filt;
+	/* Calculate the gradient of the Sawtooth */
+	m = (uint16_t) ( 4095 / DACBUFFERSIZE);
 
-	//enables GPIO clock for PortD
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-
-	codec_init();
-	codec_ctrl_init();
-
-	I2S_Cmd(CODEC_I2S, ENABLE);
-
-	initFilter(&filt);
-
-
-    while(1)
-    {
-
-    	if (SPI_I2S_GetFlagStatus(CODEC_I2S, SPI_I2S_FLAG_TXE))
-    	{
-    		SPI_I2S_SendData(CODEC_I2S, sample);
-
-    		//only update on every second sample to insure that L & R ch. have the same sample value
-    		if (sampleCounter & 0x00000001)
-    		{
-    			sawWave += NOTEFREQUENCY;
-    			if (sawWave > 1.0)
-    				sawWave -= 2.0;
-
-    			filteredSaw = updateFilter(&filt, sawWave);
-
-    			sample = (int16_t)(NOTEAMPLITUDE*filteredSaw);
-    		}
-    		sampleCounter++;
-    	}
-
-    	if (sampleCounter==48000)
-    	{
-    		LED_BLUE_OFF;
-
-    	}
-    	else if (sampleCounter == 96000)
-    	{
-    		LED_BLUE_ON;
-    		sampleCounter = 0;
-    	}
-
-    }
-}
-
-// a very crude FIR lowpass filter
-float updateFilter(fir_8* filt, float val)
-{
-	uint16_t valIndex;
-	uint16_t paramIndex;
-	float outval = 0.0;
-
-	valIndex = filt->currIndex;
-	filt->tabs[valIndex] = val;
-
-	for (paramIndex=0; paramIndex<8; paramIndex++)
+	/* Create wave table for sinewave */
+	for (n = 0; n<DACBUFFERSIZE; n++)
 	{
-		outval += (filt->params[paramIndex]) * (filt->tabs[(valIndex+paramIndex)&0x07]);
+		DACBuffer1[n] = (uint16_t)((4095+1)/2.0)*( sin( M_TWOPI*n/DACBUFFERSIZE) + 1.0 );
 	}
 
-	valIndex++;
-	valIndex &= 0x07;
+	for (n = 0; n<DACBUFFERSIZE; n++)
+	{
+		DACBuffer2[n] = (uint16_t)((255-n)*m);
+	}
 
-	filt->currIndex = valIndex;
+	for (n = 0; n<DACBUFFERSIZE; n++)
+	{
+		if(n<DACBUFFERSIZE/2)
+			DACBuffer3[n]=0;
+		else
+			DACBuffer3[n]=4095;
+	}
+	for (n = 0; n<DACBUFFERSIZE; n++)
+		{
+			DACBuffer4[n] = (DACBuffer1[n]+DACBuffer2[n]+DACBuffer3[n])/3;
+		}
 
-	return outval;
+	/* Calculate frequency of timer */
+	fTimer = WAVEFREQ * DACBUFFERSIZE;
+
+	/* Calculate Tick Rate */
+	timerFreq = TIMER_CLOCK / TIMER6_PRESCALER; /* Timer tick is in Hz */
+
+	/* Calculate period of Timer */
+	timerPeriod = (uint16_t)( timerFreq / fTimer );
+
+	/* System Clocks Configuration */
+	RCC_Configuration();
+
+	/* NVIC configuration */
+	NVIC_Configuration();
+
+	/* Configure the GPIO ports */
+	GPIO_Configuration();
+
+	/* Timer Configuration */
+	Timer_Configuration( timerPeriod, TIMER6_PRESCALER );
+
+	/* DAC Configuration */
+	DAC_Configuration();
+
+	/* DMA Config --> Takes in a pointer to the waveform buffer */
+
+	uint16_t buffer=0;
+
+	while (1)
+	{
+	  if(GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_0)){     //if button pressed
+		  DMA_Configuration(&DACBuffer1);
+		  while(GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_0));   //debounce
+		  DMA_Cmd(DMA1_Stream5, DISABLE);
+	  }
+	  else if(GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_1)){
+		  DMA_Configuration(&DACBuffer2);
+		  while(GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_1));   //debounce
+		  DMA_Cmd(DMA1_Stream5, DISABLE);
+	  }
+	  else if(GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_2)){
+		  DMA_Configuration(&DACBuffer3);
+		  while(GPIO_ReadInputDataBit(GPIOD, GPIO_Pin_2));   //debounce
+		  DMA_Cmd(DMA1_Stream5, DISABLE);
+	  }
+	  else if(STM_EVAL_PBGetState(BUTTON_USER)){
+		  DMA_Configuration(&DACBuffer4);
+		  while(STM_EVAL_PBGetState(BUTTON_USER));   //debounce
+		  DMA_Cmd(DMA1_Stream5, DISABLE);
+	  }
+	}
 }
 
-void initFilter(fir_8* theFilter)
+/**
+  * @brief  Configures the different system clocks.
+  * @param  None
+  * @retval : None
+  */
+void RCC_Configuration(void)
 {
-	uint8_t i;
+	/* Enable DMA and GPIOA Clocks */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1 | RCC_AHB1Periph_GPIOA |RCC_AHB1Periph_GPIOD, ENABLE);
 
-	theFilter->currIndex = 0;
+	/* Enable DAC1 and TIM6 clocks */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC | RCC_APB1Periph_TIM6, ENABLE);
+}
 
-	for (i=0; i<8; i++)
-		theFilter->tabs[i] = 0.0;
+/**
+  * @brief  Configures the nested vectored interrupt controller.
+  * @param  None
+  * @retval : None
+  */
+void NVIC_Configuration(void)
+{
+	NVIC_InitTypeDef NVIC_InitStructure;
 
-	theFilter->params[0] = 0.01;
-	theFilter->params[1] = 0.05;
-	theFilter->params[2] = 0.12;
-	theFilter->params[3] = 0.32;
-	theFilter->params[4] = 0.32;
-	theFilter->params[5] = 0.12;
-	theFilter->params[6] = 0.05;
-	theFilter->params[7] = 0.01;
+}
+
+
+/**
+  * @brief  Configures the different GPIO ports.
+  * @param  None
+  * @retval : None
+  */
+void GPIO_Configuration(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	/* Pack the struct */
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
+	GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+}
+
+/**
+  * @brief  Configures the DMA.
+  * @param  None
+  * @retval : None
+  */
+void DMA_Configuration( uint16_t* wavBuffer )
+{
+	DMA_InitTypeDef DMA_InitStructure;
+
+	//Initialize the structure to default values
+	DMA_StructInit(&DMA_InitStructure);
+
+	DMA_InitStructure.DMA_Channel = DMA_Channel_7;
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)(DAC_BASE + 0x08);  //DAC channel1 12-bit right-aligned data holding register (ref manual pg. 264)
+	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)wavBuffer;
+	DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
+	DMA_InitStructure.DMA_BufferSize = DACBUFFERSIZE;
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+	DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
+	DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_Full;
+	DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+	DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+
+	/* Call Init function */
+	DMA_Init(DMA1_Stream5, &DMA_InitStructure);
+
+	/* Enable DMA */
+	DMA_Cmd(DMA1_Stream5, ENABLE);
+
+}
+
+/**
+  * @brief  Configures the Timers.
+  * @param  wavePeriod (period of timer), preScaler (prescaler for timer)
+  * @retval : None
+  */
+void Timer_Configuration(uint16_t wavPeriod, uint16_t preScaler)
+{
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStruct;
+
+	/* pack Timer struct */
+	TIM_TimeBaseStruct.TIM_Period = wavPeriod-1;
+	TIM_TimeBaseStruct.TIM_Prescaler = preScaler-1;
+	TIM_TimeBaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseStruct.TIM_RepetitionCounter = 0x0000;
+
+	/* Call init function */
+	TIM_TimeBaseInit(TIM6, &TIM_TimeBaseStruct);
+
+	/* Select Timer to trigger DAC */
+	TIM_SelectOutputTrigger(TIM6, TIM_TRGOSource_Update);
+
+	/* TIM6 enable counter */
+	TIM_Cmd(TIM6, ENABLE);
+
+}
+
+/**
+  * @brief  Configures the DAC
+  * @param  None
+  * @retval : None
+  */
+void DAC_Configuration(void)
+{
+	DAC_InitTypeDef DAC_InitStruct;
+
+	/* Initialize the DAC_Trigger member */
+	DAC_InitStruct.DAC_Trigger = DAC_Trigger_T6_TRGO;
+	/* Initialize the DAC_WaveGeneration member */
+	DAC_InitStruct.DAC_WaveGeneration = DAC_WaveGeneration_None;
+	/* Initialize the DAC_LFSRUnmask_TriangleAmplitude member */
+	DAC_InitStruct.DAC_LFSRUnmask_TriangleAmplitude = DAC_LFSRUnmask_Bit0;
+	/* Initialize the DAC_OutputBuffer member */
+	DAC_InitStruct.DAC_OutputBuffer = DAC_OutputBuffer_Enable;
+
+	/* Init DAC */
+	DAC_Init(DAC_Channel_1, &DAC_InitStruct);
+
+	/* Enable DMA request */
+	DAC_DMACmd(DAC_Channel_1, ENABLE);
+
+	/* Enable DAC Channel1: Once the DAC channel1 is enabled, PA.04 is automatically connected to the DAC converter. */
+	DAC_Cmd(DAC_Channel_1, ENABLE);
+
+}
+
+/*
+ * Callback used by stm32f4_discovery_audio_codec.c.
+ * Refer to stm32f4_discovery_audio_codec.h for more info.
+ */
+void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t Size){
+  /* TODO, implement your code here */
+  return;
+}
+
+/*
+ * Callback used by stm324xg_eval_audio_codec.c.
+ * Refer to stm324xg_eval_audio_codec.h for more info.
+ */
+uint16_t EVAL_AUDIO_GetSampleCallBack(void){
+  /* TODO, implement your code here */
+  return -1;
+}
+
+
+/**
+  * @brief  Simple delay in ms --> Maybe use for debouncing?
+  * @param  integer amount of milliseconds
+  * @retval None
+  */
+void delay_ms(uint32_t milli)
+{
+	uint32_t delay = milli * 17612;              // approximate loops per ms at 168 MHz, Debug config
+	for(; delay != 0; delay--);
 }
